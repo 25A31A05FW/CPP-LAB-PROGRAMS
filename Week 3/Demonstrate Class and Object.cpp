bf96f834b03891daca2a105fd8d56499260885b0
SUDHEER <neelapalasrinu02@gmail.com>
2026-03-12 15:09:32 +0530

    Demonstrate Class and Object.cpp

---
+++ Week 3/Demonstrate Class and Object.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int roll;
+
+    void getData()
+    {
+        cout << "Enter name and roll: ";
+        cin >> name >> roll;
+    }
+
+    void display()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Roll: " << roll << endl;
+    }
+};
+
+int main()
+{
+    Student s;   // object
+
+    s.getData();
+    s.display();
+
+    return 0;
+}
